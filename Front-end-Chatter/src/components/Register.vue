f44e7a34ad8e670dd28b3e0ff005451c34de7c44
~Jens <jensblijenburg@gmail.com>
2023-04-01 13:35:56 +0200

    login and register

---
+++ Front-end-Chatter/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="registerStructure">
+      <div class="register">
+          <h1>Chatter</h1>
+          <form class="registerPart">
+              <div class="mb-3">
+                  <input v-model="username" id="inputUsername" type="text" class="form-control" placeholder="Username" />
+              </div>
+              <div class="mb-3">
+                  <input v-model="email" type="email" class="form-control" id="inputEmail" placeholder="Email" />
+              </div>
+              <div class="mb-3">
+                  <input v-model="password" type="password" class="form-control" id="inputPassword" placeholder="Password" />
+              </div>
+              <div class="mb-3">
+                  <input v-model="password" type="password" class="form-control" id="inputConfirmPassword" placeholder="Confirm Password" />
+              </div>
+             
+              <button type='button' class="btn btn-primary">Submit</button>
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+@import '../assets/css/register.css';
+</style>
